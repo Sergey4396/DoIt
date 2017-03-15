030acfca5bbf96cf32f8b8e4a1de6bc5207acea4
Sergey <344396@mail.ru>
2017-03-16 01:55:12 +0300

    Первые сомнительные действия повторенные, после которых появились три строчки и около важной знак восклицания. Работает.

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by 1234 on 16.03.17.
+//  Copyright © 2017 1234. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+var name = ""
+var important = false
+}
